5577094b5043368ee6de3e25c21f2f3385b2eb72
Arkadiikor <arkadykorlotyanu@gmail.com>
2024-06-13 17:24:51 +0300

    Add files via upload

---
+++ scr/components/product.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export function Product (){
+    return(
+        <div
+        className='border py-2 px-4 rounded flex flex-col items-center justify-center mb-2'
+        >Hellow world</div>
+    )
+}
