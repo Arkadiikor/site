9ef13e47526a777dec61c104aa90db3c5687d69b
Arkadiikor <arkadykorlotyanu@gmail.com>
2024-06-13 17:24:18 +0300

    Add files via upload

---
+++ scr/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {type} from "node:os";
+import {Product} from "./components/product";
+
+
+function App() {
+    return(
+        <div className = "container mx-auto max-w-2xl pt-5">
+            <Product/>
+
+        </div>
+    )
+}
+
+export default App;
